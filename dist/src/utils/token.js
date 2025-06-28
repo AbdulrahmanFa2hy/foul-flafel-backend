cdaf5d0a6f6a9be032de2288f2620b4dfc8cf075
AbdulrahmanFa2hy <abdelrhman.fa2hy@gmail.com>
2025-06-28 17:29:28 +0300

    fix shift

--- dist/src/utils/token.js
+++ dist/src/utils/token.js
@@ -6,8 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 class CreateTokens {
     constructor() {
-        this.accessToken = (id, role) => jsonwebtoken_1.default.sign({ _id: id, role }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
-        this.resetToken = (id) => jsonwebtoken_1.default.sign({ _id: id }, process.env.JWT_SECRET_RESET, { expiresIn: process.env.JWT_EXPIRE_RESET });
+        this.accessToken = (id, role) => {
+            return jsonwebtoken_1.default.sign({ _id: id, role }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE || "1d" });
+        };
+        this.resetToken = (id) => {
+            return jsonwebtoken_1.default.sign({ _id: id }, process.env.JWT_SECRET_RESET, { expiresIn: process.env.JWT_EXPIRE_RESET || "15m" });
+        };
     }
 }
 const createTokens = new CreateTokens();
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,11 +1,21 @@
 import Jwt from "jsonwebtoken";
 
 class CreateTokens {
-    accessToken = (id: any, role: string) =>
-        Jwt.sign({_id: id, role}, process.env.JWT_SECRET!, {expiresIn: process.env.JWT_EXPIRE})
-    
-    resetToken = (id: any) =>
-        Jwt.sign({_id: id}, process.env.JWT_SECRET_RESET!, {expiresIn: process.env.JWT_EXPIRE_RESET})
+  accessToken = (id: any, role: string): string => {
+    return Jwt.sign(
+      { _id: id, role },
+      process.env.JWT_SECRET as string,
+      { expiresIn: process.env.JWT_EXPIRE || "1d" } as any
+    );
+  };
+
+  resetToken = (id: any): string => {
+    return Jwt.sign(
+      { _id: id },
+      process.env.JWT_SECRET_RESET as string,
+      { expiresIn: process.env.JWT_EXPIRE_RESET || "15m" } as any
+    );
+  };
 }
 
 const createTokens = new CreateTokens();
